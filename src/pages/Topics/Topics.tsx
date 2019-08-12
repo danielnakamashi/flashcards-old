23fe194cdce8f80ae081a54e8dbf7930fc57cc58
Daniel Nakamashi <dnakamashi@gmail.com>
2019-08-12 15:38:34 -0300

    Add/remove card

--- src/pages/Topics/Topics.tsx
+++ src/pages/Topics/Topics.tsx
@@ -2,7 +2,9 @@ import React, { useState, useCallback } from 'react';
 import { Box } from 'grommet/components/Box';
 import { FormField } from 'grommet/components/FormField';
 import { TextInput } from 'grommet/components/TextInput';
-import { adjust } from 'ramda';
+import { Button } from 'grommet/components/Button';
+import { Add, Subtract } from 'grommet-icons';
+import { adjust, remove } from 'ramda';
 
 export interface Card {
   word: string;
@@ -10,7 +12,7 @@ export interface Card {
 }
 
 const useInputTopic = (initialValue: string) => {
-  const [topicTitle, setTopicTitle] = useState('');
+  const [topicTitle, setTopicTitle] = useState(initialValue);
   const handleTopicChange = useCallback(e => {
     setTopicTitle(e.target.value);
   }, []);
@@ -18,12 +20,17 @@ const useInputTopic = (initialValue: string) => {
   return { topicTitle, handleTopicChange };
 };
 
-const useInputCards = (initialValue: Card[]) => {
+const useInputCards = (initialValue: Card[], blankCard: Card) => {
   const [cards, setCards] = useState(initialValue);
   const handleCardTextChange = useCallback(
     (cardPropertyToChange: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { value, dataset } = event.currentTarget;
-      const index = parseInt(dataset.index || '0', 10);
+      const { value, dataset } = event.target;
+
+      if (!dataset.index) {
+        return;
+      }
+
+      const index = parseInt(dataset.index, 10);
       setCards(cards => {
         const setDefinition = (value: string) => (card: Card) => ({ ...card, [cardPropertyToChange]: value });
         return adjust(index, setDefinition(value), cards);
@@ -31,29 +38,43 @@ const useInputCards = (initialValue: Card[]) => {
     },
     [],
   );
+  const addNewCard = useCallback(() => {
+    setCards(cards => [...cards, { ...blankCard }]);
+  }, [blankCard]);
+  const removeCard = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+    const { dataset } = event.target as HTMLButtonElement;
 
-  return { cards, handleCardTextChange };
+    if (!dataset.index) {
+      return;
+    }
+
+    const index = parseInt(dataset.index, 10);
+    setCards(cards => remove(index, 1, cards));
+  }, []);
+
+  return { cards, handleCardTextChange, addNewCard, removeCard };
 };
 
 const Topic: React.FC = () => {
   const { topicTitle, handleTopicChange } = useInputTopic('');
-  const emptyCard: Card = { word: '', definition: '' };
-  const { cards, handleCardTextChange } = useInputCards([emptyCard]);
+  const blankCard: Card = { word: '', definition: '' };
+  const { cards, handleCardTextChange, addNewCard, removeCard } = useInputCards([{ ...blankCard }], { ...blankCard });
 
   return (
-    <Box direction="column">
+    <Box direction="column" margin={{ horizontal: 'small' }}>
       <FormField label="Topics Title">
         <TextInput value={topicTitle} onChange={handleTopicChange} />
       </FormField>
       {cards.map((card: Card, index: number) => {
         return (
-          <Box direction="row" key={index}>
+          <Box direction="row" key={index} margin={{ vertical: 'small' }} gap="small">
             <TextInput
               placeholder="Word"
               value={card.word}
               data-index={index}
               onChange={handleCardTextChange('word')}
             />
+            <Button label={<Subtract />} alignSelf="center" gap="small" data-index={index} onClick={removeCard} />
             <TextInput
               placeholder="Definition"
               value={card.definition}
@@ -63,6 +84,7 @@ const Topic: React.FC = () => {
           </Box>
         );
       })}
+      <Button icon={<Add />} label="Add Card" alignSelf="center" margin={{ vertical: 'small' }} onClick={addNewCard} />
     </Box>
   );
 };
