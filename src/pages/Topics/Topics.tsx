20ce2777dc3f4e0ba1e585e50b52177635ab3357
Daniel Nakamashi <dnakamashi@gmail.com>
2019-08-12 00:19:21 -0300

    Refactor Topics page

--- src/pages/Topics/Topics.tsx
+++ src/pages/Topics/Topics.tsx
@@ -2,39 +2,67 @@ import React, { useState, useCallback } from 'react';
 import { Box } from 'grommet/components/Box';
 import { FormField } from 'grommet/components/FormField';
 import { TextInput } from 'grommet/components/TextInput';
+import { adjust } from 'ramda';
 
-const TopicTitleInput: React.NamedExoticComponent<{
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}> = React.memo(props => <TextInput {...props} />);
+export interface Card {
+  word: string;
+  definition: string;
+}
 
-const Topic: React.FC = () => {
+const useInputTopic = (initialValue: string) => {
   const [topicTitle, setTopicTitle] = useState('');
   const handleTopicChange = useCallback(e => {
     setTopicTitle(e.target.value);
   }, []);
-  const [words, setWords] = useState(['']);
-  const handleWordChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, dataset } = event.currentTarget;
-    setWords(words => {
-      const copyWords = [...words];
+
+  return { topicTitle, handleTopicChange };
+};
+
+const useInputCards = (initialValue: Card[]) => {
+  const [cards, setCards] = useState(initialValue);
+  const handleCardTextChange = useCallback(
+    (cardPropertyToChange: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value, dataset } = event.currentTarget;
       const index = parseInt(dataset.index || '0', 10);
-      copyWords[index] = value;
-      return copyWords;
-    });
-  }, []);
+      setCards(cards => {
+        const setDefinition = (value: string) => (card: Card) => ({ ...card, [cardPropertyToChange]: value });
+        return adjust(index, setDefinition(value), cards);
+      });
+    },
+    [],
+  );
+
+  return { cards, handleCardTextChange };
+};
+
+const Topic: React.FC = () => {
+  const { topicTitle, handleTopicChange } = useInputTopic('');
+  const emptyCard: Card = { word: '', definition: '' };
+  const { cards, handleCardTextChange } = useInputCards([emptyCard]);
+
   return (
     <Box direction="column">
       <FormField label="Topics Title">
-        <TopicTitleInput value={topicTitle} onChange={handleTopicChange} />
+        <TextInput value={topicTitle} onChange={handleTopicChange} />
       </FormField>
-      <Box direction="row">
-        {words.map((word: string, index: number) => {
-          return (
-            <TextInput placeholder="Word" key={index} value={word} data-index={index} onChange={handleWordChange} />
-          );
-        })}
-      </Box>
+      {cards.map((card: Card, index: number) => {
+        return (
+          <Box direction="row" key={index}>
+            <TextInput
+              placeholder="Word"
+              value={card.word}
+              data-index={index}
+              onChange={handleCardTextChange('word')}
+            />
+            <TextInput
+              placeholder="Definition"
+              value={card.definition}
+              data-index={index}
+              onChange={handleCardTextChange('definition')}
+            />
+          </Box>
+        );
+      })}
     </Box>
   );
 };
